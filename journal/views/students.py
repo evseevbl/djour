9a0a787c4aa30618a9eda6d25d1f67cc6c0666a4
Boris <bevseev@ozon.ru>
2019-10-18 13:21:28 +0300

    wip on stats

--- journal/views/students.py
+++ journal/views/students.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from journal.managers.context import with_context
-from journal.models import Student
+from journal.models import Student, Mark, Subject
 
 from django.views.decorators.csrf import ensure_csrf_cookie
 
@@ -32,12 +32,28 @@ def students(request):
 @login_required
 @ensure_csrf_cookie
 def student(request, student_id):
-    ls: [Student] = Student.objects.filter(id=student_id)
+    st: [Student] = Student.objects.filter(id=student_id).first()
+
+    subjs = Subject.objects.filter(lesson__mark__student_id=student_id).prefetch_related('lesson_set')
+    print(subjs)
+    for s in subjs:
+        get_avg_for_subject(s, student_id)
+
+
     return render(
         request,
         "journal/marks/student.html",
         with_context({
             # "user_id": user_id,
-            "student": ls[0],
+            "student": st,
         })
     )
+
+
+
+def get_avg_for_subject(subject, student_id):
+    marks = Mark.objects.filter(lesson__subject=subject, student_id=student_id).prefetch_related('student__mark_set__lesson', 'lesson__subject')
+    for m in marks:
+        m: Mark = m
+        print(m.val, m.student_id)
+    pass
