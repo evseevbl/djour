1d5423c64fed1bd31e50140c4e2add5bb2099945
Boris <bevseev@ozon.ru>
2019-11-01 15:24:09 +0300

    model translation

--- journal/apps.py
+++ journal/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class JournalConfig(AppConfig):
     name = 'journal'
+
+    def ready(self):
+        print('app started')
+        # todo константы
+        pass
+        # startup code here
