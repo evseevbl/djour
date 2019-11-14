ac80b8fd333ebed02e18b52c1c0865107e4044ed
Boris <bevseev@ozon.ru>
2019-11-14 22:11:45 +0300

    test

--- api/views/penalties.py
+++ api/views/penalties.py
@@ -6,7 +6,6 @@ from api.forms import LessonForm
 from journal.models import Squad, Lesson, StudentAttendance, Mark
 
 
-
 def add_penalty(request):
     print(request)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
