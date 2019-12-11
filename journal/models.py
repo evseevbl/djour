28ff5c94683ea44cdc4f48e8641321e985c58582
Valeriy Klyukin <mr.clyukin@gmail.com>
2019-12-11 13:55:24 +0300

    back from admin

--- journal/models.py
+++ journal/models.py
@@ -7,6 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect
 from image_cropping import ImageRatioField
 
 
